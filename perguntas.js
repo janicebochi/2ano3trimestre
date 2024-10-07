4ae8c69537522002558f35f57ad2bd89bd0691fb
JANICE BOCHI <160953167+janicebochi@users.noreply.github.com>
2024-10-07 11:49:28 -0300

    imagem

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'conhecimentos gerais ',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual a montanha mais alta do mundo?',
+    'O Monte Everest tem 8.848 metros de altitude e localiza-se no Nepal, um país asiático que faz fronteira com a China e com a Índia.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Onde se localiza Machu Picchu? a) Colômbia b) Peru c) China d) Bolívia e) Índia',
+    'Peru'
+)
