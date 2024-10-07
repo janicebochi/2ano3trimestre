fecf37821cda954e37d9d3b321eb79564a8715fe
JANICE BOCHI <160953167+janicebochi@users.noreply.github.com>
2024-10-07 12:02:12 -0300

    a2

--- perguntas.js
+++ perguntas.js
@@ -15,21 +15,3 @@ criaCartao(
     'Qual a montanha mais alta do mundo?',
     'O Monte Everest tem 8.848 metros de altitude e localiza-se no Nepal, um país asiático que faz fronteira com a China e com a Índia.'
 )
-
-criaCartao(
-    'conhecimentos gerais',
-    'Onde se localiza Machu Picchu? a) Colômbia b) Peru c) China d) Bolívia e) Índia,
-    'Peru'
-)
-
-criaCartao(
-    'conhecimentos gerais',
-    'Que país tem o formato de uma bota?,
-    'Itália.'
-)
-
-criaCartao(
-    'conhecimentos gerais',
-    'O que é mais pesado: 1 quilo de algodão ou 1 quilo de ferro?,
-    'Ambos têm o mesmo peso'
-)
